5a28881382415f34b78bbb27a9581ff682ee5371
lokimenethill <xdjtrancemetal@hotmail.com>
2023-08-24 16:18:51 -0600

    29Ago2023

--- PythonCodes/Block.py
+++ PythonCodes/Block.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 import numpy as np
@@ -5,11 +6,37 @@ import numpy as np
 
 
 bloque = 0
-maxB = 3
+maxB = 5
 valido = 0
+
+#Lee wallet pública
+public = open("public_1.key").read()[27:148] 
+public = public[:64] + public[65:]
+
 
 while(bloque < maxB):
 
+    #Agregando la transacción de recompensa
+    #Obtener los balances del minero
+    wallets = json.load(open("wallets.json", "r+"))
+    balanceM = int(wallets[public])
+    
+    #Generando la transacción
+    tran = {"Monto":"50",
+            "Envia":"Recompensa",
+    			"Recibe":public,
+    			"Nuevo_Balance_Recibe":str(balanceM+50)}
+    
+    with open('blockchain/'+str(bloque)+'.json', 'r+') as b:
+        data = json.load(b)
+        b.seek(0)        # Posición del archivo al inicio
+        data['Transacciones'] = [tran]
+        json.dump(data, b, indent=4)
+        b.truncate()
+    b.close()
+    
+
+    #Buscando el nonce que genere un hash válido
     valido = 0
     while(valido == 0):
     
@@ -39,9 +66,18 @@ while(bloque < maxB):
                 b.seek(0)        # Posición del archivo al inicio
                 json.dump(data, b, indent=4)
                 b.truncate()     # Eliminar parte restante
+            b.close()
         
         else: valido = 1
     
+    #-> Actualiza balance del minero en wallets
+    with open('wallets.json', 'r+') as w:
+        wallets = json.load(w)
+        wallets[public] = str(balanceM + 50) #Modifica el balance agregando la recompensa
+        w.seek(0)        # Posición del archivo al inicio
+        json.dump(wallets, w, indent=4)
+        w.truncate()     # Eliminar parte restante
+    w.close()
     
     #Escribir nuevo bloque
     with open('blockchain/'+str(bloque+1)+'.json', 'w') as nb:
@@ -49,6 +85,7 @@ while(bloque < maxB):
         data['Hash'] = digesto
         json.dump(data, nb, indent=4)
         nb.truncate()     # Eliminar parte restante
+    nb.close()
     
     #Cierra el archivo
     block.close()
